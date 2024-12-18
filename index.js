970de1867586b8506f30b8ac38e5167dfe97b893
eankhbayar <eankhbayarr@gmail.com>
2024-12-18 18:25:11 +0800

    basic functions done

--- index.js
+++ index.js
@@ -4,34 +4,24 @@ const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const cors = require('cors');
 var parser = require('xml2json');
 
 const app = express();
 const PORT = 3000;
 const JWT_SECRET = 'pZd6pmFzw2BO39+N9r+DmwKx0QlZBuXaivHxT2L4nKU='; // Replace with a secure key in production
 app.use(bodyParser.json());
+app.use(cors());
 
-fs.readFile('./events.xml', function(err, data) {
-	var json = parser.toJson(data);
-	console.log("to json ->", json);
+mongoose.connect('mongodb://localhost:27017/project');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+db.once('open', () => {
+	console.log('MongoDB connected');
 });
-
-fs.readFile('./venues.xml', function(err, data) {
-	var json = parser.toJson(data);
-	console.log("to json ->", json);
-});
-
-fs.readFile('./holiday.xml', function(err, data) {
-	var json = parser.toJson(data);
-	console.log("to json ->", json);
-});
-
-fs.readFile('./eventDates.xml', function(err, data) {
-	var json = parser.toJson(data);
-	console.log("to json ->", json);
-});
-
-mongoose.connect('mongodb://localhost:27017/authdb');
 
 const userSchema = new mongoose.Schema({
 	username: { type: String, required: true, unique: true },
@@ -40,6 +30,27 @@ const userSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model('User', userSchema);
+
+// Location schema
+const locationSchema = new mongoose.Schema({
+	id: String,
+	locationname: String,
+	latitude: Number,
+	longitude: Number,
+});
+
+const Location = mongoose.model('Location', locationSchema);
+
+// Event schema
+const eventSchema = new mongoose.Schema({
+	title: String,
+	venue: String,
+	datetime: String,
+	description: String,
+	presenter: String,
+});
+
+const Event = mongoose.model('Event', eventSchema);
 
 const generateToken = (user) => {
 	return jwt.sign(
@@ -115,6 +126,66 @@ const authenticateToken = (req, res, next) => {
 // Protected Route Example
 app.get('/protected', authenticateToken, (req, res) => {
 	res.json({ message: 'This is a protected route.', user: req.user });
+});
+
+// Reset database and initialize data
+const initializeData = async () => {
+	await Location.deleteMany({});
+	await Event.deleteMany({});
+
+	let locationIds = [];
+
+	fs.readFile('./venues.xml', async (err, data) => {
+		if (err) throw err;
+		const json = JSON.parse(parser.toJson(data));
+		const venues = json.venues.venue;
+
+		const locations = venues.map((venue) => ({
+			id: venue.id,
+			locationname: venue.venuee,
+			latitude: parseFloat(venue.latitude) || 0,
+			longitude: parseFloat(venue.longitude) || 0,
+		}));
+
+		locationIds = locations.map((location) => location.id);
+
+		await Location.insertMany(locations);
+		console.log('Locations initialized');
+	});
+
+	fs.readFile('./events.xml', async (err, data) => {
+		if (err) throw err;
+		const json = JSON.parse(parser.toJson(data));
+		const events = json.events.event;
+
+		const eventList = events
+        .filter(event => locationIds.includes(event.venueid) && !(typeof event.desce === 'object' && Object.keys(event.desce).length === 0))
+        .map((event) => {
+            return {
+                id: event.id,
+                title: event.titlee,
+                venue: event.venueid,
+                datetime: event.predateE,
+                description: event.desce,
+                presenter: event.presenterorge,
+            };
+        });
+
+		await Event.insertMany(eventList);
+		console.log('Events initialized');
+	});
+};
+
+initializeData();
+
+app.get('/locations', authenticateToken, async (req, res) => {
+	const locations = await Location.find({});
+	res.json(locations);
+});
+
+app.get('/events', authenticateToken, async (req, res) => {
+	const events = await Event.find({});
+	res.json(events);
 });
 
 app.listen(PORT, () => {
